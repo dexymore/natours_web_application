1d9add02bd5cc499c2ea4359d393c40bca1d6346
ahmedFATHY <af773942@gmail.com>
2023-07-24 20:06:53 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,10 +29,10 @@ const signToken = function (id) {
 };
 const createSendToken = function(user,statuscode,res){
   const token = signToken(user._id);
-const cookiesOptions={
-expires:new Date(Date.now()+process.env.JWT_EXPIRES_IN_COOKIE*24*60*60*1000),
-httpOnly:true
-}
+  const cookiesOptions = {
+    expires: new Date(Date.now() + (process.env.JWT_EXPIRES_IN_COOKIE * 1000)), // JWT_EXPIRES_IN_COOKIE should be in seconds
+    httpOnly: true
+  };
 if(process.env.NODE_ENV==="production") cookiesOptions.secure=true
 
 res.cookie('jwt',token,cookiesOptions)
